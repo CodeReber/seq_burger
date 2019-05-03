18f58077d620d4d0af208220428f06027bac9926
codereber <jamesmpyle@gmail.com>
2019-05-03 17:55:06 -0400

    update

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+### Schema
+
+CREATE DATABASE burgers_db;
+USE `burgers_db`;
+
+-- Create the table burgers
+CREATE TABLE `burgers`
+(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`burger_name` VARCHAR(255) NOT NULL,
+	`devoured` BOOLEAN DEFAULT false,
+	`date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Bacon', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('WHopper', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('BigMac', false, CURRENT_TIMESTAMP);
